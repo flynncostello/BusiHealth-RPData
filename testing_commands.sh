acc6c28a7dc2c8481ff37657de481c93ee9f87d2
Flynn Costello <fcos0917@uni.sydney.edu.au>
2025-05-01 00:23:28 +1000

    ensures logging goes to azure log stream

--- testing_commands.sh
+++
@@ -1,7 +0,0 @@
-docker build -t rpdata-scraper:test .
-docker run --rm -it --platform=linux/amd64 --shm-size=2g -p 8000:8000 rpdata-scraper:test
-
-
-docker run -it --rm --platform=linux/amd64 --shm-size=2g rpdata-scraper:test bash
-# Then try:
-/opt/chrome/chrome --headless --disable-gpu --remote-debugging-port=9222 https://www.google.com
