0189e4208c4390a24eff191069c9f83a4512460b
Flynn Costello <fcos0917@uni.sydney.edu.au>
2025-05-12 19:44:30 +1000

    newest fix

--- rpdata_scraper/scraper/rpdata_base.py
+++ rpdata_scraper/scraper/rpdata_base.py
@@ -14,6 +14,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from chrome_utils import setup_chrome_driver
 
 
+
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
